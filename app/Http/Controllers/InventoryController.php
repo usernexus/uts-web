cda6f129002f38297412c96335a731f2492da215
Salman El Farisi <dev.salman.farisi@gmail.com>
2016-03-21 14:06:15 +0700

    initial commit

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Inventory;
+
+class InventoryController extends Controller
+{
+    public function __construct(Inventory $inventory)
+    {
+        $this->inventory = $inventory;
+    }
+
+    public function index(Request $request)
+    {
+        return response()->view('index');
+    }
+
+    public function form(Request $request)
+    {
+        return response()->view('form');
+    }
+}
